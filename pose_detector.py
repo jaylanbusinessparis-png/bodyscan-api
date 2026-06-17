00a8cb420b3b6efc3480f54d66f68615f72a662b
Jaylan Joseph <jaylanjoseph@MacBook-Pro-de-Jaylan.local>
2026-06-17 19:50:31 +0200

    Fix mediapipe import

--- pose_detector.py
+++ pose_detector.py
@@ -9,7 +9,7 @@ from __future__ import annotations
 
 import cv2
 import mediapipe as mp
-from mediapipe.python.solutions import pose as mp_pose
+import mediapipe.solutions.pose as mp_pose
 
 import numpy as np
 from dataclasses import dataclass, field
